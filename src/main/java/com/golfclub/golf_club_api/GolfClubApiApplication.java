e3a8c0418485c0ee630b8679da60a422893f0921
brwmercer <brwmercer@gmail.com>
2025-07-31 09:58:58 -0230

    Initial Commit

---
+++ src/main/java/com/golfclub/golf_club_api/GolfClubApiApplication.java
@@ -0,0 +1,13 @@
+package com.golfclub.golf_club_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfClubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfClubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/golfclub/golf_club_api/GolfClubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golfclub.golf_club_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfClubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
